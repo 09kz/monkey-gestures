9e9c2c1f84d803cc452610d560dda7345afe4cfe
09kz <k.zydek@aol.com>
2026-08-13 21:03:38 +0200

    folder structure

---
+++ dataset/creation_tool.py
@@ -0,0 +1,5 @@
+# script for creating dataset
+# captures images from webcam every 5 seconds, runs hand_landmarker model on them, saves
+
+def main():
+    return
---
+++ main.py
@@ -0,0 +1,8 @@
+# inferencja, obrazek, ladne gui z ustawieniami, real time detection z obrazkami malpek, model lokalnie
+
+def main():
+    print("Hello from monkey-gestures!")
+
+
+if __name__ == "__main__":
+    main()
